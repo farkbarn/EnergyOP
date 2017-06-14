964065642a6b1af92e9bd0e64183bdfe78830f16
Frank Barrera (farkbarn) <frankbarrerag@gmail.com>
2017-06-14 11:45:33 -0400

    agregando cambios en jquery
    Signed-off-by: Frank Barrera (farkbarn) <frankbarrerag@gmail.com>

--- assets/js/actions.js
+++ assets/js/actions.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 	//cambio de datos en idioma
+		//ES_es
 		const $es='/es/';
+
+		//EN_en
 		const $en='/en/';
 	//	$url=window.location.href;
 	//	$path=window.location.path;
@@ -8,6 +11,11 @@ $(document).ready(function() {
 		$path=$(location).attr('path');
 
 		if ($path==$es) {
+			$('#btn_about_us').text('Sobre Nosotros');
+			$('#btn_contact').text('Contacto');
+		}
+
+		if ($path==$en) {
 			$('#btn_about_us').text('Sobre Nosotros');
 			$('#btn_contact').text('Contacto');
 		}
